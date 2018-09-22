c4c9fcd52e70e3cc21fd7be6dbb0d25171fecc02
Deborah Castiglioni <debjgorton@gmail.com>
2018-09-22 17:01:45 -0400

    Update state to Main.js instead of App

--- src/App.js
+++ src/App.js
@@ -5,24 +5,12 @@ import Search from './Search';
 import Main from './Main';
 
 import './App.css'
-import * as BooksAPI from './BooksAPI';
+
 
 class BooksApp extends React.Component {
-  //Add state for fetching Books
-  state = {
-    library: []
-  }
-  
-  componentDidMount() {
-    BooksAPI.getAll().then((library) => {
-      this.setState({ library: library })
-    })
-  }
-
 
 //Render the HTML
   render() {
-    console.log(this.state.library);
     return (
       <div className="app">
         <Main />
