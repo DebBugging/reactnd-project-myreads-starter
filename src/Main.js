623a1f17ea1407cb6b65acb04cc0f8934794f5b5
Deborah Castiglioni <debjgorton@gmail.com>
2018-09-23 00:47:54 -0400

    Fetch the data from the API to show up

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import Shelf from "./Shelf";
+import * as BooksAPI from "./BooksAPI";
 
 class Main extends Component {
+  //Add state for fetching Books
+  state = {
+    library: []
+  };
+  //Fetch books from the BooksAPI
+  componentDidMount() {
+    BooksAPI.getAll().then(library => {
+      this.setState({ library: library });
+    });
+  }
+
   render() {
     return (
       <div className="list-books">
@@ -10,7 +22,7 @@ class Main extends Component {
         </div>
         <div className="list-books-content">
           <div>
-              <Shelf />
+            <Shelf library={this.state.library} />
           </div>
         </div>
         <div className="open-search">
--- src/Shelf.js
+++ src/Shelf.js
@@ -3,15 +3,20 @@ import Book from "./Book";
 
 class Shelf extends Component {
   render() {
+    console.log(this.props.library);
     return (
       <div>
         <div className="bookshelf">
           <h2 className="bookshelf-title">Currently Reading</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
+              {this.props.library
+                .filter(book => book.shelf === "currentlyReading")
+                .map(book => (
+                  <li key={book.id}>
+                    <Book book={book} />
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
@@ -19,9 +24,13 @@ class Shelf extends Component {
           <h2 className="bookshelf-title">Want to Read</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
+              {this.props.library
+                .filter(book => book.shelf === "wantToRead")
+                .map(book => (
+                  <li key={book.id}>
+                    <Book book={book} />
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
@@ -29,9 +38,13 @@ class Shelf extends Component {
           <h2 className="bookshelf-title">Read</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              <li>
-                <Book />
-              </li>
+              {this.props.library
+                .filter(book => book.shelf === "read")
+                .map(book => (
+                  <li key={book.id}>
+                    <Book book={book} />
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
